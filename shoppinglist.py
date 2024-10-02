1bd485d104f29e9253a1fd8f08818b4e0a5979f9
144droit <indieohren@gmail.com>
2024-10-02 17:37:39 +0200

    created function to add items to shoppinglist

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,3 +1,10 @@
 # Definieren der Variable shoppinglist
-shoppinglist = [1,2,3]
-print(shoppinglist)
+shoppinglist = ["Apfel","Banane","Kirsche"]
+
+# Funktion add-item
+
+item = input("Welcher Artikel soll der Einkaufsliste hinzugefügt werden?")
+def add_item():
+    shoppinglist.append(item)
+    print(item, "wurde der Einkaufsliste hinzugefügt")
+add_item()
