e050f68b9e56c58b6ed5ec270c231f859b16b074
144droit <indieohren@gmail.com>
2024-10-02 19:22:51 +0200

    edited existing functions for better performance

--- shoppinglist.py
+++ shoppinglist.py
@@ -4,14 +4,19 @@ shoppinglist = []
 # Funktion zum Hinzufügen eines Artikels
 item = input("Welcher Artikel soll der Einkaufsliste hinzugefügt werden?")
 def add_item():
-    shoppinglist.append(item)
-    print(item, "wurde der Einkaufsliste hinzugefügt")
+    if not item:
+        print("Das kann nicht hinzugefügt werden")
+    else:
+        shoppinglist.append(item)
+        print(item, "wurde der Einkaufsliste hinzugefügt")
 add_item()
 
 # Funktion zum Anzeigen der Einkaufsliste
 def show_shoppinglist():
-    if not shoppinglist:
-        print("Deine Einkaufsliste leer")
+    if shoppinglist:
+        print("Deine Einkaufsliste:")
+        for item in shoppinglist:
+            print(item)
     else:
-        print(f"Deine Einkaufsliste beinhaltet: {shoppinglist}!")
+        print("Deine Einkaufsliste ist leer")
 show_shoppinglist()
