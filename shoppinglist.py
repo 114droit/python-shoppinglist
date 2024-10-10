930232d65f235fbd7605529863c8c324a63fb12c
144droit <indieohren@gmail.com>
2024-10-10 16:37:48 +0200

    added sqlite-database and CRUD functions for managing shopping list

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,41 +1,87 @@
-# Definieren der Variable shoppinglist
-shoppinglist = []
+import sqlite3
 
-# Funktion zum Hinzufügen eines Artikels
-def add_item():
-    item = input("Welcher Artikel soll der Einkaufsliste hinzugefügt werden?")
-    if not item:
-        print("Das kann nicht hinzugefügt werden")
-    else:
-        shoppinglist.append(item)
-        print(item, "wurde der Einkaufsliste hinzugefügt")
+# Verbindung zu sqlite-DB (Falls keine db vorhanden, wird diese erstellt)
+conn = sqlite3.connect('shoppinglist.db')
 
-# Funktion zum Anzeigen der Einkaufsliste
-def show_shoppinglist():
-    if shoppinglist:
-        print("Deine Einkaufsliste:")
-        for item in shoppinglist:
-            print(item)
+# Erstellen eines cursor (Zeiger?) um sql-Befehl durchzuführen
+cursor = conn.cursor()
+
+# Erstellen von Tabellen in shoppinglist.db
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS groceries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(32) NOT NULL,
+    amount INTEGER NOT NULL,
+    price FLOAT NOT NULL
+    );
+''')
+
+# Funktion zum Hinzufügen eines Artikels (CREATE)
+def add_item(name, amount, price):
+    cursor.execute('''
+    INSERT INTO groceries (name, amount, price) VALUES (?, ?, ?)
+    ''', (name, amount, price))
+    conn.commit()
+    print(f"{name} wurde hinzugefügt.\n")
+
+# Funktion zum Anzeigen der Einkaufsliste (READ)
+def show_items():
+    cursor.execute('SELECT id, name, amount, price FROM groceries')
+    items = cursor.fetchall()
+    if not items:
+        print("\nDeine Einkaufsliste ist leer\n")
     else:
-        print("Deine Einkaufsliste ist leer")
+        for id, name, amount, price in items:
+            print(f"Artikel: {name}, Menge: {amount}, Preis: {price}, id: {id}")
+
+# Funktion zum aktualisieren der Artikel der Einkaufsliste (UPDATE)
+def update_item(id, name, amount, price):
+    cursor.execute('''
+    UPDATE groceries SET name = ?, amount = ?, price = ?
+    WHERE id = ?
+    ''', (name, amount, price, id))
+    conn.commit()
+    print(f"Der Artikel mit der id {id} wurde aktualisiert.\n")
+
+# Funktion zum löschen eines Atikels (DELETE)
+def delete_item(id):
+    cursor.execute('''
+    DELETE FROM groceries WHERE id = ?
+    ''', (id))
+    conn.commit()
+    print(f"Der Artikel mit der id {id} wurde gelöscht.\n")
 
 # Main Funktion
 def main():
     while True:
-        print("\n")
-        print("-----Einkaufsliste-----")
-        print("1. Artikel zur Einkaufsliste hinzufügen")
-        print("2. Einkaufsliste anzeigen")
-        print("3. Programm beenden")
-        choice = input("Was möchtest du tun?(1/2/3)")
+        print("\n-----Einkaufsliste-----\n")
+        print("1. Artikel hinzufügen")
+        print("2. Artikel bearbeiten")
+        print("3. Artikel löschen")
+        print("4. Einkaufsliste anzeigen")
+        print("5. Programm beenden")
+        choice = input("\nWas möchtest du tun?(1/2/3)\n")
         if choice == "1":
-            add_item()
+            name = input("Was möchtest du der Einkaufsliste hinzufügen?\n")
+            amount = input("Wieviele?\n")
+            price = input("(Ungefährer) Preis des einzelnen Artikels?\n")
+            add_item(name, amount, price)
         elif choice == "2":
-            show_shoppinglist()
+            id = input("Welchen Artikel (id, zu finden in der Einkaufsliste) möchtest du bearbeiten? \n")
+            name = input("Aktualisierter Name des Artikels:\n")
+            amount = input("Aktualisierte Menge:\n")
+            price = input("Aktualisierter Preis:\n")
+            update_item(id, name, amount, price)
         elif choice == "3":
-            print("Programm wird beendet! Auf Wiedersehen")
+            id = input("Welchen Artikel (id) möchtest du löschen?\n")
+            delete_item(id)
+        elif choice == "4":
+            show_items()
+        elif choice == "5":
+            print("Programm wird beendet. Auf Wiedersehen")
             break
-        elif choice:
-            print("Ungültige Auswahl. Bitte wähle 1,2 0der 3")
+        else:
+            print("Ungültige Auswahl. Bitte wähle 1,2,3,4 oder 5\n")
+
 if __name__ == "__main__":
     main()
